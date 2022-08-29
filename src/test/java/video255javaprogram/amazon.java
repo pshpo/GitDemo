bb4ff400143af0d363051ac3c6ce60d4c86fc04d
pooja <shetty.pooja742@gmail.com>
2022-08-29 16:26:18 -0700

    Y commited

--- src/test/java/video255javaprogram/amazon.java
+++ src/test/java/video255javaprogram/amazon.java
@@ -26,7 +26,6 @@ public class amazon {
 				System.out.println(a[i]+" is repeated "+k+" times");
 				if(k==1)
 				{
-					System.out.println("**************************************");
 					System.out.println("Unique number is :"+a[i]);
 				}
 			}
