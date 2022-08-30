a8bb604ccb8a022bb0514586b12408ecec0b20db
pooja <shetty.pooja742@gmail.com>
2022-08-29 17:34:32 -0700

    Stuff master commited

---
+++ src/test/java/video255javaprogram/demo.java
@@ -0,0 +1,11 @@
+package video255javaprogram;
+
+public class demo {
+
+	public static void main(String[] args)
+	{
+		System.out.println("****************************************");
+		System.out.println("****************************************");
+	}
+
+}
